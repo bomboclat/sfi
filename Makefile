13acfe0b003f417426efdcd5021668756dcfe34e
Andrea Galletti <andrew.gall92@gmail.com>
2021-03-08 11:46:35 +0100

    Add first release

---
+++ Makefile
@@ -0,0 +1,58 @@
+# libsfi
+# (c) 2021, 2022 Andrea Galletti
+
+VERSION = 1.0
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+# includes and libs
+INCS = -I. -I/usr/include
+LIBS = -L/usr/lib
+# flags
+CFLAGS = -O0 -Wall -Werror -ansi ${INCS} -DVERSION=\"${VERSION}\"
+LDFLAGS = ${LIBS}
+# srcs / objs
+SRC    = sfi.c
+OBJ    = ${SRC:.c=.o}
+
+# Main targets
+all: sfi
+
+sfi: ${OBJ}
+	@echo LD $@
+	@${CC} -o $@ ${OBJ} ${LDFLAGS}
+
+# Targets deps
+.c.o:
+	@echo CC $<
+	@${CC} -c ${CFLAGS} $<
+
+# Helper targets
+clean:
+	@echo cleaning
+	@rm -f sfi ${OBJ} ${LIBOBJ} sfi-${VERSION}.tar.gz
+
+dist: clean
+	@echo creating dist tarball
+	@mkdir -p sfi-${VERSION}
+	@cp -R LICENSE Makefile config.mk sfi.1 ${SRC} sfi-${VERSION}
+	@tar -zcf sfi-${VERSION}.tar sfi-${VERSION}
+	@rm -rf sfi-${VERSION}
+
+install: all
+	@echo installing executable file to ${DESTDIR}${PREFIX}/bin
+	@mkdir -p ${DESTDIR}${PREFIX}/bin
+	@cp -f sfi ${DESTDIR}${PREFIX}/bin
+	@chmod 755 ${DESTDIR}${PREFIX}/bin/sfi
+	@echo installing manual page to ${DESTDIR}${MANPREFIX}/man1
+	@mkdir -p ${DESTDIR}${MANPREFIX}/man1
+	@sed "s/VERSION/${VERSION}/g" < sfi.1 > ${DESTDIR}${MANPREFIX}/man1/sfi.1
+	@chmod 644 ${DESTDIR}${MANPREFIX}/man1/sfi.1
+
+uninstall:
+	@echo removing executable file from ${DESTDIR}${PREFIX}/bin
+	@rm -f ${DESTDIR}${PREFIX}/bin/sfi
+	@echo removing manual page from ${DESTDIR}${MANPREFIX}/man1
+	@rm -f ${DESTDIR}${MANPREFIX}/man1/sfi.1
+
+.PHONY: all clean dist install uninstall
---
+++ sfi.c
@@ -0,0 +1,233 @@
+/*
+ * ==================================================
+ *
+ *       Filename:  sfi.c
+ *    Description:  Simple file importer written in C 
+ *
+ *        Created:  01/25/2021 09:57:15 PM
+ *       Compiler:  cc
+ *         Author:  Andrea Galletti ()
+ *
+ *       Copyright (c) 2021 Andrea Galletti
+ *       See LICENSE for further informations
+ *
+ * ==================================================
+ */
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef VERSION
+#define VERSION "\"No version information\""
+#endif
+#define LENGTH(x) sizeof(x)/sizeof(x[0])
+
+typedef const char * (*Parser)(const char *start, const char *end, int);
+
+static const char * doreplace(const char *start, const char *end, int newchunk);
+
+static const char *wrappers[2] = { "{{", "}}" };
+static const char *replace[][2] = {
+	{ "\\{",	"{" },
+	{ "\\}",	"}" }
+};
+static const Parser parsers[] = { doreplace };
+
+char *
+usage() {
+	return "Usage %s [OPTION] [file]\n"
+		"-v	print the version and exit\n"
+		"-h	print this help and exit\n";
+}
+
+void
+eprintf(const char *format, ...) {
+	va_list ap;
+	
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
+
+void *
+erecalloc(void *p, size_t size) {
+	void *res;
+	if (p) {
+		res = realloc(p, size);
+	}
+	else {
+		res = calloc(1, size);
+	}
+	if (!res)
+		eprintf("fatal; could not malloc() %u bytes\n", size);
+	return res;
+}
+
+char *
+read_file(const char *file) {
+	int readed;
+	size_t len = 0, bsize = 2 * BUFSIZ;
+	char *buffer = NULL;
+	FILE *source = stdin;
+
+	if (file && !(source = fopen(file, "r")))
+		eprintf("Cannot open file `%s`\n", file);
+	/* allocate base buffer space */
+	buffer = erecalloc(buffer, bsize);
+	while ((readed = fread(&buffer[len], 1, BUFSIZ, source))) {
+		len += readed;
+		if (len + BUFSIZ + 1 > bsize) {
+			/* increment buffer size if needed */
+			bsize += BUFSIZ;
+			buffer = erecalloc(buffer, bsize);
+		}
+	}
+	/* check fread errors */
+	if(ferror(source)){
+		eprintf("fread: file: %s: %s\n", file, strerror(errno));
+	}
+	buffer[len] = '\0';
+	fclose(source);
+	return buffer;
+}
+
+const char *
+find_string(const char *buffer, const char *to_find) {
+	size_t len;
+	const char *start, *end;
+	len = strlen(to_find);
+	end = buffer + strlen(buffer);
+	/* iterate over char pointers */
+	for (start = buffer; start < end; start++) {
+		if (strncmp(to_find, start, len) == 0) {
+			/* check if the first char is escaped */
+			if (start != buffer && *(start-1) == '\\')
+				continue;
+			return start;
+		}
+	}
+	return NULL;
+}
+
+char *
+copy_chunk(const char *start, const char *end) {
+	char *buffer = NULL;
+	size_t blen = end - start;
+	if (blen > 0) {
+		/* copy the string chunk */
+		buffer = erecalloc(buffer, blen);
+		memcpy(buffer, start, blen * sizeof(char));
+		buffer[blen] = '\0';
+		return buffer;
+	}
+	return NULL;
+}
+
+void
+print_chunk(const char *start, const char *end) {
+	char *buffer = copy_chunk(start, end);
+	/* print the chunk and free */
+	if (buffer) {
+		fputs(buffer, stdout);
+		free(buffer);
+	}
+}
+
+const char *
+doreplace(const char *start, const char *end, int newchunk) {
+	const char *found = NULL, *cursor = NULL;
+	int i, len = LENGTH(replace), pos;
+
+	for (i = 0; i < len; i++) {
+		found = find_string(start, replace[i][0]);
+		if (found && (found < cursor || !cursor)) {
+			cursor = found;
+			pos = i;
+		}
+	}
+	if (cursor && newchunk == 1)
+		return cursor;
+	if (cursor && newchunk == 0) {
+		fputs(replace[pos][1], stdout);
+		return cursor + strlen(replace[pos][0]);
+	}
+	return NULL;
+}
+
+void
+process_import(const char *buffer) {
+	const char *start = buffer, *end = NULL, *found, *cursor;
+	char *file, *filename;
+	int i, j;
+	Parser cur_parser;
+	for (i = 0; *start != '\0'; i++) {
+		end = find_string(start, wrappers[i % 2]);
+		if (!end) {
+			end = start + strlen(start);
+		}
+		else if (start != buffer && i % 2 > 0) {
+			/* skip previous wrapper from start*/
+			start += strlen(wrappers[(i - 1) % 2]);
+			filename = copy_chunk(start, end);
+			file = read_file(filename);
+			/* recursion here!! */
+			process_import(file);
+			free(filename);
+			free(file);
+			/* skip current wrapper from end */
+			start = end += strlen(wrappers[i % 2]);
+		}
+
+		do {
+			cursor = found = NULL;
+			/* iterate over parsers and choose the nearest to start */
+			for (j = 0; j < LENGTH(parsers); j++) {
+				found = parsers[j](start, end, 1);
+				if (found && (!cursor || found  < cursor)) {
+					cur_parser = parsers[j];
+					cursor = found;
+				}
+			}
+			if (cursor) {
+				print_chunk(start, cursor);
+				start = cur_parser(start, end, 0);
+			}
+		}
+		while (found);
+
+		print_chunk(start, end);
+		start = end;
+	}
+}
+
+int
+main(int argc, char *argv[]) {
+	int i;
+        char *program_name = argv[0];
+	char *filename = NULL;
+	
+	/* filter arguments */
+	for(i=1; i < argc; i++) {
+		if (!strcmp("-v", argv[i])) {
+			printf("Simple template engine %s (C) Andrea Galletti\n",
+				VERSION);
+			return EXIT_SUCCESS;
+		}
+		else if (!strcmp("-h", argv[i])) {
+			printf(usage(), program_name);
+			return EXIT_SUCCESS;
+		} 
+		else if (i == argc-1 && argv[i][0] != '-') {
+			filename = argv[i];
+		}
+		else {
+			fprintf(stderr, usage(), argv[0]);
+			eprintf("\nInvalid parameter: %s\n", argv[i]);
+		}
+	}
+	process_import(read_file(filename));
+	return EXIT_SUCCESS;
+}
